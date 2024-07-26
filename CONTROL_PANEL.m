663d1d143933944c149378ba34fb560ef1b25fb3
LoicGerber <loic.gerber.2@unil.ch>
2024-07-26 12:28:41 +0200

    Removed inputDir

--- CONTROL_PANEL.m
+++ CONTROL_PANEL.m
@@ -11,30 +11,30 @@
 clc, clear
 
 % All directories
-rawDir            = 'path/to/raw/data';                         % Path to raw data
-inputDir          = 'path/to/saved/inputs';                     % Path to saved input data, will be automatically created
-outputDir         = 'path/to/outputs';                          % Path to results, will be automatically created
-optiWeightsDir    = 'path/to/optimised/weights/matrix.mat';     % Path to optimised weights matrix .mat file (if available, otherwise '')
-maskDir           = 'path/to/binary/mask';                      % Path to binary mask delimiting the area to generate with the pixel-based approach (.tif image)
+rawDir            = 'path\to\raw\data';                         % Path to raw data
+outputDir         = 'path\to\outputs';                          % Path to results, will be automatically created
+optiWeightsDir    = 'path\to\optimised\weights\matrix.mat';     % OPTIONAL - Path to optimised weights matrix .mat file (if available, otherwise '')
+maskDir           = 'path\to\binary\mask';                      % OPTIONAL - Path to binary mask delimiting the area to generate with the pixel-based approach (.tif image)
 
 % ConvertStructureToInputs
 targetVar         = ["Et"];                         % Variables to be generated, with ["example1","example2"]
 climateVars       = ["Tavg","Tmin","Tmax","Pre"];   % Input variables considered for the data generation, with ["example1","example2"]
 addVars           = ["sm","twsa"];                  % Additional input variables, with ["example1","example2"], if empty use []
-normMethods       = [1,1,1,4];                      % Method of normalisation of the climate data. 1 = MinMax, 2 = Q10-Q90, 3 = log(x+0.01), 4 = log(x>0)
+normMethods       = [1,1,1,4];                      % Method of normalisation of the climate data. 0 = No normalisation, 1 = MinMax, 2 = Q10-Q90, 3 = log(x+1), 4 = mean dist Hamming & log(x(x>0)+1)
 maxThreshold      = 30;                             % Days, max threshold for closest additional variables attribution
 QdateStart        = 19500101;                       % YYYYMMDD - Start of the Generation period
 QdateEnd          = 19791231;                       % YYYYMMDD - End of the Generation period
 LdateStart        = 19800101;                       % YYYYMMDD - Start of the Learning period
 LdateEnd          = 20201231;                       % YYYYMMDD - End of the Learning period
 outputTime        = 1;                              % Image generation timestep: 1 = DAILY, 2 = MONTHLY, 3 = DEKADAL
+targetDim         = 2;                              % Dimension of target variable: 1 = 1D, 2 = 2D
 
 % KNNDataGeneration
 pixelWise         = true;       % Pixel-wise kNN (true), or domain-wise kNN (false)
 shortWindow       = 5;          % Number of days to consider for the short climate window
 longWindow        = 20;         % Number of days to consider for the long climate window (total days, including shortWindow)
 nbImages          = 10;         % K, number of days to consider for the generation of images
-metricKNN         = 2;          % 1 = RMSE, 2 = MAE, 3 = Manhattan distance, 4 = Euclidean distance
+metricKNN         = 2;          % 1 = RMSE, 2 = MAE, 3 = Manhattan distance, 4 = Euclidean distance, 5 = SPEM
 daysRange         = 90;         % Range of possible learning days (before and after) the query date's DOY
 
 % GenerateSynImages
@@ -44,11 +44,11 @@ outputType        = 2;          % Output data file type, 1 = GeoTIFF, 2 = NetCDF
 coordRefSysCode   = 4326;       % Coordinate reference system code, WGS84 = 4326, https://epsg.org/home.html
 
 % Functions switches
-parallelComputing = false;      % true = parallel computing ON,         false = parallel computing OFF
-netCDFtoInputs    = true;       % true = create inputs,                 false = load inputs
-createGenWeights  = true;       % true = create generic weights,        false = load optimised weights
-kNNsorting        = true;       % true = create sorted data,            false = load sorted data
-generateImage     = true;       % true = image generation ON,           false = image generation OFF
+parallelComputing = false;      % true = parallel computing ON,  false = parallel computing OFF
+netCDFtoInputs    = true;       % true = create inputs,          false = load inputs
+createGenWeights  = true;       % true = create generic weights, false = load optimised weights
+kNNsorting        = true;       % true = create sorted data,     false = load sorted data
+generateImage     = true;       % true = image generation ON,    false = image generation OFF
 
 % Bootstrap switches (NOT INFLUENCED BY VALIDATION SWITCHES)
 bootstrap         = false;      % true = bootstrap ON,                  false = bootstrap OFF
@@ -58,7 +58,7 @@ bsSaveAll         = false;      % true = saves all bootstrap ensembles, false = 
 validationPrep    = false;      % true = validation preparation ON, false = validation preparation OFF (!!! BYPASSES PREVIOUS SWITCHES !!!)
 validation        = false;      % true = validation ON,             false = validation OFF (!!! BYPASSES PREVIOUS SWITCHES !!!)
 metricViz         = false;      % true = visualisation ON,          false = visualisation OFF
-metricV           = 1;          % 1 = RMSE, 2 = SPEM, 3 = SPAEF, 4 = Symmetric Phase-only Matched Filter-based Absolute Error Function (SPOMF)
+metricV           = 1;          % 1 = RMSE, 2 = SPEM, 3 = SPAEF, 4 = KGE (1D ONLY), 5 = NSE (1D ONLY)
 
 % Bayesian optimisation switches
 optimPrep         = false;      % true = optimisation preparation ON, false = optimisation preparation OFF (!!! BYPASSES PREVIOUS SWITCHES !!!)
@@ -70,7 +70,7 @@ nbOptiRuns        = 50;         % Number of runs for the Bayesian optimisation a
 [geoRef,climateData,queryDates,learningDates,refValidation,additionalVars, ...
     Weights,sortedDates,synImages,validationMetric,optimisedWeights] = ...
     MAIN(...
-    rawDir,inputDir,outputDir,optiWeightsDir,maskDir,targetVar,climateVars,addVars,normMethods,QdateStart,QdateEnd,LdateStart,LdateEnd,outputTime, ...
+    rawDir,outputDir,optiWeightsDir,maskDir,targetVar,climateVars,addVars,normMethods,QdateStart,QdateEnd,LdateStart,LdateEnd,outputTime,targetDim, ...
     maxThreshold,shortWindow,longWindow,daysRange,nbImages,metricKNN,ensemble,generationType,outputType,coordRefSysCode,parallelComputing, ...
     netCDFtoInputs,createGenWeights,kNNsorting,generateImage,bootstrap,bsSaveAll,validationPrep,validation,pixelWise, ...
     metricViz,metricV,optimPrep,saveOptimPrep,optimisation,nbOptiRuns);
--- MAIN.m
+++ MAIN.m
@@ -1,6 +1,6 @@
 function [geoRef,climateData,queryDates,learningDates,refValidation,additionalVars, ...
     Weights,sortedDates,synImages,validationMetric,optimisedWeights] = MAIN(...
-    rawDir,inputDir,outputDir,optiWeightsDir,maskDir,targetVar,climateVars,addVars,normMethods,QdateStart,QdateEnd,LdateStart,LdateEnd,outputTime,targetDim, ...
+    rawDir,outputDir,optiWeightsDir,maskDir,targetVar,climateVars,addVars,normMethods,QdateStart,QdateEnd,LdateStart,LdateEnd,outputTime,targetDim, ...
     maxThreshold,shortWindow,longWindow,daysRange,nbImages,metricKNN,ensemble,generationType,outputType,coordRefSysCode,parallelComputing, ...
     netCDFtoInputs,createGenWeights,kNNsorting,generateImage,bootstrap,bsSaveAll,validationPrep,validation,pixelWise, ...
     metricViz,metricV,optimPrep,saveOptimPrep,optimisation,nbOptiRuns)
@@ -20,46 +20,49 @@ end
 %% Reading the data needed for ranking learning dates using "KNNDataSorting" Function
 disp('--- 1. READING DATA ---')
 
+inDir = strcat(outputDir,'\inputData');
+outDir = strcat(outputDir,'\output');
+
 if netCDFtoInputs == true || optimPrep == true || validationPrep == true
     disp('Formatting input data...')
-    rawData        = convertRawDataToStructure(targetVar,targetDim,climateVars,addVars,rawDir,inputDir);
+    rawData        = convertRawDataToStructure(targetVar,targetDim,climateVars,addVars,rawDir,inDir);
     disp('Extracting georeference informations...')
     if targetDim ~= 1
-        geoRef = extractGeoInfo(targetVar,coordRefSysCode,rawDir,inputDir);
+        geoRef = extractGeoInfo(targetVar,coordRefSysCode,rawDir,inDir);
     else
         geoRef = [];
     end
     disp('Extracting climate informations...')
-    climateData    = extractClimateData(climateVars,rawData,normMethods,QdateStart,QdateEnd,LdateStart,LdateEnd,longWindow,inputDir);
+    climateData    = extractClimateData(climateVars,rawData,normMethods,QdateStart,QdateEnd,LdateStart,LdateEnd,longWindow,inDir);
     disp('Extracting Learning dates...')
-    learningDates  = convertStructureToLearningDates(targetVar,LdateStart,LdateEnd,QdateStart,QdateEnd,rawData,climateData,targetDim,optimPrep,inputDir);
+    learningDates  = convertStructureToLearningDates(targetVar,LdateStart,LdateEnd,QdateStart,QdateEnd,rawData,climateData,targetDim,optimPrep,inDir);
     disp('Extracting Query dates...')
-    [queryDates,learningDates,refValidation] = convertStructureToQueryDates(targetVar,targetDim,QdateStart,QdateEnd,learningDates,climateData,maxThreshold,validationPrep,optimPrep,outputTime,inputDir,outputDir);
+    [queryDates,learningDates,refValidation] = convertStructureToQueryDates(targetVar,targetDim,QdateStart,QdateEnd,learningDates,climateData,maxThreshold,validationPrep,optimPrep,outputTime,inDir,outDir);
     disp('Extracting additional variables...')
-    additionalVars = extractAdditionalVars(addVars,rawData,climateData,QdateStart,QdateEnd,LdateStart,LdateEnd,maxThreshold,inputDir);
+    additionalVars = extractAdditionalVars(addVars,rawData,climateData,QdateStart,QdateEnd,LdateStart,LdateEnd,maxThreshold,inDir);
 elseif netCDFtoInputs == false && validationPrep == false
     disp('Loading QueryDates.mat file...')
-    queryDates     = load(fullfile(inputDir,'queryDates.mat'));
+    queryDates     = load(fullfile(inDir,'queryDates.mat'));
     queryDates     = queryDates.queryDates;
     disp('Loading LearningDates.mat file...')
-    learningDates  = load(fullfile(inputDir,'learningDates.mat'));
+    learningDates  = load(fullfile(inDir,'learningDates.mat'));
     learningDates  = learningDates.learningDates;
     disp('Loading climateData.mat file...')
-    climateData    = load(fullfile(inputDir,'climateData.mat'));
+    climateData    = load(fullfile(inDir,'climateData.mat'));
     climateData    = climateData.climateData;
     disp('Loading additionalVars.mat file...')
-    additionalVars = load(fullfile(inputDir,'additionalVars.mat'));
+    additionalVars = load(fullfile(inDir,'additionalVars.mat'));
     additionalVars = additionalVars.additionalVars;
     disp('Loading GeoRef.mat file...')
     if targetDim ~= 1
-        geoRef         = load(fullfile(inputDir,'GeoRef.mat'));
+        geoRef         = load(fullfile(inDir,'GeoRef.mat'));
         geoRef         = geoRef.geoRef;
     else
         geoRef = [];
     end
     if optimisation == true || validation == true || metricViz == true
         disp('Loading refValidation.mat file...')
-        refValidation = load(fullfile(inputDir,'refValidation.mat'));
+        refValidation = load(fullfile(inDir,'refValidation.mat'));
         refValidation = refValidation.refValidation;
     else
         refValidation = [];
@@ -67,7 +70,7 @@ elseif netCDFtoInputs == false && validationPrep == false
 end
 if createGenWeights == true || optimPrep == true || optimisation == true
     disp('Creating generic weights...')
-    Weights = createWeights(targetVar,climateVars,addVars,inputDir);
+    Weights = createWeights(targetVar,climateVars,addVars,inDir);
 elseif createGenWeights == false
     disp('Loading optimisedWeights.mat file...')
     optimisedWeights = load(optiWeightsDir);
@@ -82,17 +85,17 @@ disp('--- 2. KNN DATA SORTING ---')
 % Generate ranked Learning Dates for each Query Date
 if kNNsorting == true || validationPrep == true || optimPrep == true
     if pixelWise == false
-        sortedDates = kNNDataSorting(targetVar,climateVars,addVars,queryDates,learningDates,climateData,additionalVars,normMethods,shortWindow,longWindow,daysRange,Weights,nbImages,metricKNN,optimPrep,saveOptimPrep,parallelComputing,inputDir);
+        sortedDates = kNNDataSorting(targetVar,climateVars,addVars,queryDates,learningDates,climateData,additionalVars,normMethods,shortWindow,longWindow,daysRange,Weights,nbImages,metricKNN,optimPrep,saveOptimPrep,parallelComputing,inDir);
     else
-        sortedDates = pixelWise_kNNDataSorting(maskDir,targetVar,climateVars,addVars,queryDates,learningDates,climateData,additionalVars,normMethods,shortWindow,longWindow,daysRange,Weights,nbImages,metricKNN,optimPrep,saveOptimPrep,parallelComputing,inputDir);
+        sortedDates = pixelWise_kNNDataSorting(maskDir,targetVar,climateVars,addVars,queryDates,learningDates,climateData,additionalVars,normMethods,shortWindow,longWindow,daysRange,Weights,nbImages,metricKNN,optimPrep,saveOptimPrep,parallelComputing,inDir);
     end
 elseif kNNsorting == false && validationPrep == false && (optimPrep == false && optimisation == false)
     disp('Loading sortedDates.mat file...')
-    sortedDates = load(fullfile(inputDir,'KNNSorting.mat'));
+    sortedDates = load(fullfile(inDir,'KNNSorting.mat'));
     sortedDates = sortedDates.sortedDates;
 elseif optimPrep == false && optimisation == true
     disp('Loading KNNDistances.mat file...')
-    sortedDates = load(fullfile(inputDir,'KNNDistances.mat'));
+    sortedDates = load(fullfile(inDir,'KNNDistances.mat'));
     sortedDates = sortedDates.sortedDates;
 end
 
@@ -103,16 +106,16 @@ disp('--- 3. SYNTHETIC IMAGES GENERATION ---')
 
 if (generateImage == true || validation == true) && optimisation == false
     if pixelWise == false
-        synImages = generateSynImages(targetVar,targetDim,learningDates,sortedDates,geoRef,outputDir,generationType,validation,optimisation,bootstrap,bsSaveAll,nbImages,ensemble,outputType);
+        synImages = generateSynImages(targetVar,targetDim,learningDates,sortedDates,geoRef,outDir,generationType,validation,optimisation,bootstrap,bsSaveAll,nbImages,ensemble,outputType);
     else
-        synImages = pixelWise_generateSynImages(maskDir,targetVar,learningDates,sortedDates,geoRef,outputDir,generationType,validation,optimisation,bootstrap,bsSaveAll,nbImages,ensemble,outputType);
+        synImages = pixelWise_generateSynImages(maskDir,targetVar,learningDates,sortedDates,geoRef,outDir,generationType,validation,optimisation,bootstrap,bsSaveAll,nbImages,ensemble,outputType);
     end
 elseif optimisation == true && validation == false
     disp('Optimisation run, synthetic image generation skipped...')
     synImages = [];
 elseif metricViz == true
     disp('Loading synValidation.mat file...')
-    synImages = load(fullfile(outputDir,'synValidation.mat'));
+    synImages = load(fullfile(outDir,'synValidation.mat'));
     synImages = synImages.synImages;
 elseif generateImage == false && validation == false
     disp('Synthetic image generation skipped...')
@@ -125,8 +128,8 @@ disp('--- 3. SYNTHETIC IMAGES GENERATION DONE ---')
 if (validation == true || metricViz == true) && optimisation == false
     disp('--- 4. VALIDATION ---')
     
-    validationMetric = validationMetrics(targetVar,targetDim,metricV,optimisation,refValidation,synImages,bootstrap,ensemble,outputDir);
-    visualiseMetrics(nbImages,pixelWise,targetVar,targetDim,refValidation,synImages,validationMetric,sortedDates,metricV,metricKNN,LdateStart,LdateEnd,QdateStart,QdateEnd,daysRange,bootstrap,outputDir);
+    validationMetric = validationMetrics(targetVar,targetDim,metricV,optimisation,refValidation,synImages,bootstrap,ensemble,outDir);
+    visualiseMetrics(nbImages,pixelWise,targetVar,targetDim,refValidation,synImages,validationMetric,sortedDates,metricV,metricKNN,LdateStart,LdateEnd,QdateStart,QdateEnd,daysRange,bootstrap,outDir);
     
     disp('--- 4. VALIDATION DONE ---')
 else
@@ -150,7 +153,7 @@ if optimisation == true
     % Set up the Bayesian optimization
     fun = @(x)computeObjectiveOptim(x.(1), x.(2), x.(3), x.(4), x.(5), x.(6), x.(7), x.(8), x.(9), ...
         targetVar, addVars, learningDates, sortedDates, refValidation, saveOptimPrep, nbImages, ...
-        geoRef, generationType, bootstrap, ensemble, metricV, validation, optimisation, inputDir, outputDir);
+        geoRef, generationType, bootstrap, ensemble, metricV, validation, optimisation, inDir, outDir);
     % Run the Bayesian optimization
     %if parallelComputing == true
     %    results = bayesopt(fun,bayesWeights,'Verbose',0,'AcquisitionFunctionName','expected-improvement-plus','MaxObjectiveEvaluations',nbOptiRuns,'UseParallel',true);
@@ -161,7 +164,7 @@ if optimisation == true
     disp('  Saving optimisedWeights.mat...')
     optimisedWeights = results.XAtMinObjective;
     optimisedWeights = array2table(table2array(optimisedWeights) ./ sum(table2array(optimisedWeights)),'VariableNames', results.XAtMinObjective.Properties.VariableNames)
-    save(fullfile(inputDir,'optimisedWeights.mat'), 'optimisedWeights', '-v7.3','-nocompression');
+    save(fullfile(inDir,'optimisedWeights.mat'), 'optimisedWeights', '-v7.3','-nocompression');
     
     disp('--- 4. OPTIMISATION DONE ---')
 else
